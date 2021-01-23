9aac06ed3a04f06a32c377af05bbca4b656c936b
vanessa <vanessapcbarros@gmail.com>
2021-01-23 17:46:55 +0100

    feat: add show and hide form functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,12 @@
 "use script";
 
 const menuButton = document.querySelector(".mobile-nav-button");
+const signUpButton = document.querySelector(".sign-up-button");
+const closeButton = document.querySelector(".close-form");
 
 menuButton.addEventListener("click", showMenu);
+signUpButton.addEventListener("click", showForm);
+closeButton.addEventListener("click", hideForm);
 
 let isShowingMenu = false;
 
@@ -14,4 +18,13 @@ function showMenu() {
     return;
   } else menu.style.display = "block";
   isShowingMenu = true;
+}
+
+function showForm() {
+  const form = document.querySelector("#form-display");
+  return form.className = "show-contact-form";
+}
+function hideForm() {
+  const form = document.querySelector("#form-display");
+  form.className = "contact-form";
 }
